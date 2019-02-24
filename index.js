ea697e5286b22346252fa137e9c40a50fe160945
ardier16 <ardier16@gmail.com>
2019-02-24 21:20:35 +0200

    Use nodemon for starting up the app
    + nodemon
    + format code

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 let express = require('express')
-
 let bodyParser = require('body-parser')
-
 let mongoose = require('mongoose')
-
 let app = express()
-
 let apiRoutes = require("./api-routes")
 
 app.use(bodyParser.urlencoded({
@@ -13,14 +9,14 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/grate')
+mongoose.connect('mongodb://localhost/grate', {
+  useNewUrlParser: true,
+})
 let db = mongoose.connection;
 
 let port = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('Hello World with Express'))
-
-app.use('/api', apiRoutes)
+app.use('/', apiRoutes)
 
 app.listen(port, function () {
   console.log("Running gRate on port " + port)
