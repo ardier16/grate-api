aeae0b26dc616a7059f43116f4561a918ed6bdd9
ardier16 <ardier16@gmail.com>
2019-02-28 23:46:15 +0200

    Restructure the project
    + use babel core
    + create /src folder
    + rename server.js -> index.js
    - babel index.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Secret key for encrypting the tokens
-  secret: 'YOUR_SECRET_KEY',
-  // MongoDB connection string
-  dbConnectionString: 'mongodb://127.0.0.1:27017/grate_db',
-  // Default application port
-  port: 8080,
-}
